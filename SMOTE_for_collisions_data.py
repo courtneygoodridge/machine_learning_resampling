a436af65c811d85ea410d9aa390fc4c740ceedcb
courtneygoodridge <c.m.goodridge@leeds.ac.uk>
2024-01-18 16:59:19 +0000

    data manipulation for vehicle and gaze data

--- SMOTE_for_collisions_data.py
+++ SMOTE_for_collisions_data.py
@@ -14,6 +14,7 @@ import statistics as stats
 from collections import Counter
 import seaborn as sns
 import math as mt
+import re
 
 # modules for computing SMOTE
 import imblearn
@@ -23,14 +24,38 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from imblearn.over_sampling import SMOTE
 
-## loading vehicle crash data
+## VEHICLE DATA
 vehicle_data = pd.read_csv(r'C:\Users\psccgoo\OneDrive - University of Leeds\ITS\Work package 6.5 (HiDrive)\TME study\Data\Vehicle data\Processed data\data_for_analysis\takeover.responses.critical.csv')
 
-## filter out participants
+## filter out participants and drop NAs
 vehicle_data_filtered = vehicle_data \
     .loc[vehicle_data['ppid'] != 34] \
         .loc[vehicle_data['ppid'] != 37] \
-            .loc[vehicle_data['ppid'] != 47]
+            .loc[vehicle_data['ppid'] != 47] \
+                .dropna()
+
+# create new variable for collision
+vehicle_data_filtered['collision_new'] = np.select([vehicle_data_filtered['collision'] == True, vehicle_data_filtered['collision'] == False], [1, 0])
+
+# PARTICIPANT DATA 
+participant_info = pd.read_csv(r'C:\Users\psccgoo\OneDrive - University of Leeds\gaze_entropy_heterogenous\data\participant_info.csv')
+participant_info = participant_info.iloc[:, 0:5] \
+    .rename(columns = {participant_info.columns[2]: 'annual_mileage'}) \
+        .rename(columns = {participant_info.columns[3]: 'license_years'}) \
+            .rename(columns = {participant_info.columns[4]: 'ppid'}) # select first 5 columns, then rename mileage, license years, and ppid
+        
+# GAZE DATA
+gaze_entropy = pd.read_csv(r'C:\Users\psccgoo\OneDrive - University of Leeds\gaze_entropy_heterogenous\data\entropy.total.csv')
+
+# filtering out participants and leavign on critical trials
+gaze_entropy_filtered = gaze_entropy \
+    .loc[gaze_entropy['ppid'] != 34] \
+        .loc[gaze_entropy['ppid'] != 37] \
+            .loc[gaze_entropy['ppid'] != 47] \
+                .loc[gaze_entropy['critical'] == True]
+       
+# merging participant information and gaze data
+gaze_entropy_filtered.merge(participant_info, how = "left", on = 'ppid')
 
 
 # load in critical data
